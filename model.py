75d3c9a3df4a5353e5170337c03d3e14652858be
Alex Cabrera <alcamiz@outlook.com>
2023-04-16 11:04:15 -0500

    Add models

--- model.py
+++ model.py
@@ -1,35 +1,94 @@
 from torch.nn import Module
 from torch import nn
+from enum import Enum
 
+class NonLinearity(Enum):
+    RELU = 1
+    TANH = 2
+    SIGMOID = 3
+    GLU = 4
 
 class Model(Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.relu1 = nn.ReLU()
-        self.pool1 = nn.MaxPool2d(2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.relu2 = nn.ReLU()
-        self.pool2 = nn.MaxPool2d(2)
-        self.fc1 = nn.Linear(256, 120)
-        self.relu3 = nn.ReLU()
-        self.fc2 = nn.Linear(120, 84)
-        self.relu4 = nn.ReLU()
-        self.fc3 = nn.Linear(84, 10)
-        self.relu5 = nn.ReLU()
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 6, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Linear(256, 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, 10),
+            nn.ReLU(),
+        )
 
     def forward(self, x):
-        y = self.conv1(x)
-        y = self.relu1(y)
-        y = self.pool1(y)
-        y = self.conv2(y)
-        y = self.relu2(y)
-        y = self.pool2(y)
-        y = y.view(y.shape[0], -1)
-        y = self.fc1(y)
-        y = self.relu3(y)
-        y = self.fc2(y)
-        y = self.relu4(y)
-        y = self.fc3(y)
-        y = self.relu5(y)
-        return y
+        return self.layers(x)
+
+class Model2(Module):
+    def __init__(self, non_linearity=NonLinearity.RELU):
+        super(Model, self).__init__()
+
+        activation = None
+        if (non_linearity is NonLinearity.RELU):
+            activation = nn.ReLU
+        elif (non_linearity is NonLinearity.TANH):
+            activation = nn.Tanh
+        elif (non_linearity is NonLinearity.SIGMOID):
+            activation = nn.Sigmoid
+        elif (non_linearity is NonLinearity.GLU):
+            activation = nn.GLU
+
+        assert (activation == None)
+
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 6, 5),
+            activation(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(6, 16, 5),
+            activation(),
+            nn.MaxPool2d(2),
+            nn.Linear(256, 120),
+            activation(),
+            nn.Linear(120, 84),
+            activation(),
+            nn.Linear(84, 10),
+            activation(),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+    
+class Model5(Module):
+    def __init__(self, gated=False):
+        super(Model, self).__init__()
+
+        recurrent = None
+        if (gated):
+            recurrent = nn.LSTM
+        else:
+            recurrent = nn.RNN
+
+        assert (recurrent == None)
+        
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 6, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Linear(256, 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, 10),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
