c419c5e355336d2c10b43b07a9253d058a6928a9
Alex Cabrera <alcamiz@outlook.com>
2023-04-17 14:14:07 -0500

    Add recurrence/inequality tests - (And some light refactoring of training code)

--- model.py
+++ model.py
@@ -1,23 +1,33 @@
+import torch
+
 from torch.nn import Module
 from torch import nn
 from enum import Enum
 
 class NonLinearity(Enum):
-    RELU = 1
-    TANH = 2
-    SIGMOID = 3
-    GLU = 4
+    RELU = 0
+    TANH = 1
+    SIGMOID = 2
+    # GLU = 3 # (Reduces size. can't use directly).
+
+class RecType(Enum):
+    LINEAR = 0
+    RNN = 1
+    LSTM = 2
 
 class Model(Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.layers = nn.Sequential(
+        self.in_layer = nn.Sequential(
             nn.Conv2d(1, 6, 5),
             nn.ReLU(),
             nn.MaxPool2d(2),
             nn.Conv2d(6, 16, 5),
             nn.ReLU(),
             nn.MaxPool2d(2),
+        )
+
+        self.out_layer = nn.Sequential(
             nn.Linear(256, 120),
             nn.ReLU(),
             nn.Linear(120, 84),
@@ -27,11 +37,14 @@ class Model(Module):
         )
 
     def forward(self, x):
-        return self.layers(x)
+        y = self.in_layer(x)
+        y = y.view(y.shape[0], -1)
+        y = self.out_layer(y)
+        return y
 
-class Model2(Module):
+class InequalityModel(Module):
     def __init__(self, non_linearity=NonLinearity.RELU):
-        super(Model, self).__init__()
+        super(InequalityModel, self).__init__()
 
         activation = None
         if (non_linearity is NonLinearity.RELU):
@@ -40,18 +53,21 @@ class Model2(Module):
             activation = nn.Tanh
         elif (non_linearity is NonLinearity.SIGMOID):
             activation = nn.Sigmoid
-        elif (non_linearity is NonLinearity.GLU):
-            activation = nn.GLU
+        # elif (non_linearity is NonLinearity.GLU):
+        #     activation = nn.GLU
 
-        assert (activation == None)
+        assert (activation != None)
 
-        self.layers = nn.Sequential(
+        self.in_layer = nn.Sequential(
             nn.Conv2d(1, 6, 5),
             activation(),
             nn.MaxPool2d(2),
             nn.Conv2d(6, 16, 5),
             activation(),
             nn.MaxPool2d(2),
+        )
+
+        self.out_layer = nn.Sequential(
             nn.Linear(256, 120),
             activation(),
             nn.Linear(120, 84),
@@ -61,34 +77,64 @@ class Model2(Module):
         )
 
     def forward(self, x):
-        return self.layers(x)
+        y = self.in_layer(x)
+        y = y.view(y.shape[0], -1)
+        y = self.out_layer(y)
+        return y
     
-class Model5(Module):
-    def __init__(self, gated=False):
-        super(Model, self).__init__()
+class RecurrentModel(Module):
+    def __init__(self, type=RecType.LINEAR):
+        super(RecurrentModel, self).__init__()
 
-        recurrent = None
-        if (gated):
-            recurrent = nn.LSTM
-        else:
-            recurrent = nn.RNN
+        recurrence = None
+        self.h_0 = None
+        self.recurrent = False
 
-        assert (recurrent == None)
+        if (type is RecType.LINEAR):
+            recurrence = nn.Linear
+            self.recurrent = False
+        elif (type is RecType.RNN):
+            self.h_0 = torch.zeros((1,84))
+            recurrence = nn.RNN
+            self.recurrent = True
+        elif (type is RecType.LSTM):
+            self.h_0 = (torch.zeros((1,84)), torch.zeros((1,84)))
+            recurrence = nn.LSTM
+            self.recurrent = True
+
+        assert (recurrence != None)
         
-        self.layers = nn.Sequential(
+        self.in_layer = nn.Sequential(
             nn.Conv2d(1, 6, 5),
             nn.ReLU(),
             nn.MaxPool2d(2),
             nn.Conv2d(6, 16, 5),
             nn.ReLU(),
             nn.MaxPool2d(2),
+        )
+
+        self.pre_layer = nn.Sequential(
             nn.Linear(256, 120),
             nn.ReLU(),
-            nn.Linear(120, 84),
-            nn.ReLU(),
+        )
+
+        self.r_layer = recurrence(120,84)
+
+        self.pos_layer = nn.Sequential(
             nn.Linear(84, 10),
             nn.ReLU(),
         )
 
     def forward(self, x):
-        return self.layers(x)
+        y = self.in_layer(x)
+        y = y.view(y.shape[0], -1)
+
+        y = self.pre_layer(y)
+
+        if (self.recurrent):
+            y, _ = self.r_layer(y, self.h_0)
+        else:
+            y = self.r_layer(y)
+
+        y = self.pos_layer(y)
+        return y
--- train.py
+++ train.py
@@ -1,6 +1,9 @@
-from model import Model
+#! /usr/bin/env python3
+
+from model import *
 import numpy as np
 import os
+import sys
 import torch
 from torchvision.datasets import mnist
 from torch.nn import CrossEntropyLoss
@@ -8,23 +11,151 @@ from torch.optim import SGD
 from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor
 
-if __name__ == '__main__':
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+from matplotlib import pyplot
+
+EPOCHS = 20
+REPS = 3
+
+def train_inequality():
     batch_size = 256
     train_dataset = mnist.MNIST(root='./train', train=True, transform=ToTensor())
     test_dataset = mnist.MNIST(root='./test', train=False, transform=ToTensor())
     train_loader = DataLoader(train_dataset, batch_size=batch_size)
     test_loader = DataLoader(test_dataset, batch_size=batch_size)
-    model = Model().to(device)
+
+    for rep in range(REPS):
+
+        print('\n-- Starting rep #%d --\n'%(rep))
+
+        for activation in NonLinearity:
+
+            print('Training %s:'%(activation.name))
+
+            model = InequalityModel(activation)
+            sgd = SGD(model.parameters(), lr=1e-1)
+            loss_fn = CrossEntropyLoss()
+            all_epoch = EPOCHS
+            
+            accuracies = [0]*all_epoch
+
+            for current_epoch in range(all_epoch):
+                model.train()
+                for idx, (train_x, train_label) in enumerate(train_loader):
+                    train_x = train_x
+                    train_label = train_label
+                    sgd.zero_grad()
+                    predict_y = model(train_x.float())
+                    loss = loss_fn(predict_y, train_label.long())
+                    loss.backward()
+                    sgd.step()
+
+                all_correct_num = 0
+                all_sample_num = 0
+
+                model.eval()
+                for idx, (test_x, test_label) in enumerate(test_loader):
+                    test_x = test_x
+                    test_label = test_label
+                    predict_y = model(test_x.float()).detach()
+                    predict_y = torch.argmax(predict_y, dim=-1)
+                    current_correct_num = predict_y == test_label
+                    all_correct_num += np.sum(current_correct_num.numpy(), axis=-1)
+                    all_sample_num += current_correct_num.shape[0]
+
+                acc = all_correct_num / all_sample_num
+                accuracies[current_epoch] = acc
+                print('\tFinished epoch %s (%.2f)'%(current_epoch+1, acc))
+
+            print('Finished training\n')
+
+            pyplot.xlabel("Epoch")
+            pyplot.ylabel("Accuracy")
+            pyplot.plot([x+1 for x in list(range(all_epoch))], accuracies, label=activation.name)
+
+        pyplot.legend(loc='best')
+        if not os.path.isdir("plots"):
+            os.mkdir("plots")
+        pyplot.savefig('plots/inequality_%d.png'%(rep), bbox_inches='tight')
+        pyplot.clf()
+
+
+def train_recurrent():
+    batch_size = 256
+    train_dataset = mnist.MNIST(root='./train', train=True, transform=ToTensor())
+    test_dataset = mnist.MNIST(root='./test', train=False, transform=ToTensor())
+    train_loader = DataLoader(train_dataset, batch_size=batch_size)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
+    for rep in range(REPS):
+
+        print('\n-- Starting rep #%d --\n'%(rep))
+
+        for recurrence in RecType:
+
+            print('Training %s:'%(recurrence.name))
+
+            model = RecurrentModel(recurrence)
+            sgd = SGD(model.parameters(), lr=1e-1)
+            loss_fn = CrossEntropyLoss()
+            all_epoch = EPOCHS
+            accuracies = [0]*all_epoch
+
+            for current_epoch in range(all_epoch):
+                model.train()
+                for idx, (train_x, train_label) in enumerate(train_loader):
+                    train_x = train_x
+                    train_label = train_label
+                    sgd.zero_grad()
+                    predict_y = model(train_x.float())
+                    loss = loss_fn(predict_y, train_label.long())
+                    loss.backward()
+                    sgd.step()
+
+                all_correct_num = 0
+                all_sample_num = 0
+
+                model.eval()
+                for idx, (test_x, test_label) in enumerate(test_loader):
+                    test_x = test_x
+                    test_label = test_label
+                    predict_y = model(test_x.float()).detach()
+                    predict_y =torch.argmax(predict_y, dim=-1)
+                    current_correct_num = predict_y == test_label
+                    all_correct_num += np.sum(current_correct_num.numpy(), axis=-1)
+                    all_sample_num += current_correct_num.shape[0]
+
+                acc = all_correct_num / all_sample_num
+                accuracies[current_epoch] = acc
+                print('\tFinished epoch %s (%.2f)'%(current_epoch+1, acc))
+
+            print('Finished training\n')
+
+            pyplot.xlabel("Epoch")
+            pyplot.ylabel("Accuracy")
+            pyplot.plot([x+1 for x in list(range(all_epoch))], label=recurrence.name)
+
+        pyplot.legend(loc='best')
+        pyplot.title('Repetition #%d'%(rep))
+        if not os.path.isdir("plots"):
+            os.mkdir("plots")
+        pyplot.savefig('plots/recurrence_%d.png'%(rep), bbox_inches='tight')
+        pyplot.clf()
+
+def train_original():
+    batch_size = 256
+    train_dataset = mnist.MNIST(root='./train', train=True, transform=ToTensor())
+    test_dataset = mnist.MNIST(root='./test', train=False, transform=ToTensor())
+    train_loader = DataLoader(train_dataset, batch_size=batch_size)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+    model = Model()
     sgd = SGD(model.parameters(), lr=1e-1)
     loss_fn = CrossEntropyLoss()
-    all_epoch = 100
-    prev_acc = 0
+    all_epoch = EPOCHS
     for current_epoch in range(all_epoch):
         model.train()
         for idx, (train_x, train_label) in enumerate(train_loader):
-            train_x = train_x.to(device)
-            train_label = train_label.to(device)
+            train_x = train_x
+            train_label = train_label
             sgd.zero_grad()
             predict_y = model(train_x.float())
             loss = loss_fn(predict_y, train_label.long())
@@ -36,8 +167,8 @@ if __name__ == '__main__':
         model.eval()
         
         for idx, (test_x, test_label) in enumerate(test_loader):
-            test_x = test_x.to(device)
-            test_label = test_label.to(device)
+            test_x = test_x
+            test_label = test_label
             predict_y = model(test_x.float()).detach()
             predict_y =torch.argmax(predict_y, dim=-1)
             current_correct_num = predict_y == test_label
@@ -45,10 +176,31 @@ if __name__ == '__main__':
             all_sample_num += current_correct_num.shape[0]
         acc = all_correct_num / all_sample_num
         print('accuracy: {:.3f}'.format(acc), flush=True)
+
         if not os.path.isdir("models"):
             os.mkdir("models")
         torch.save(model, 'models/mnist_{:.3f}.pkl'.format(acc))
-        if np.abs(acc - prev_acc) < 1e-4:
-            break
-        prev_acc = acc
+
     print("Model finished training")
+
+def main():
+    if (len(sys.argv) < 2):
+        print("Not enough arguments")
+        return
+
+    test_num = eval(sys.argv[1])
+    if (test_num == 0):
+        train_original()
+    elif (test_num == 1):
+        pass
+    elif (test_num == 2):
+        train_inequality()
+    elif (test_num == 3):
+        pass
+    elif (test_num == 4):
+        pass
+    elif (test_num == 5):
+        train_recurrent()
+
+if __name__ == '__main__':
+    main()
