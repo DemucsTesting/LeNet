a52a46de80e87a0600104609a32c485b2e2ca576
Alex Cabrera <alcamiz@outlook.com>
2023-04-17 15:29:04 -0500

    Add lr loops - Should give additional info on Sigmoid performance

--- train.py
+++ train.py
@@ -15,6 +15,7 @@ from matplotlib import pyplot
 
 EPOCHS = 20
 REPS = 3
+LR_NUM = 3
 LR = 1e-1
 
 def train_inequality():
@@ -24,60 +25,63 @@ def train_inequality():
     train_loader = DataLoader(train_dataset, batch_size=batch_size)
     test_loader = DataLoader(test_dataset, batch_size=batch_size)
 
-    for rep in range(REPS):
+    for lr_exp in range(1, LR_NUM+1):
 
-        print('\n-- Starting rep #%d --\n'%(rep))
+        for rep in range(REPS):
 
-        for activation in NonLinearity:
+            print('\n-- Starting: lr 10^-%d, rep #%d --\n'%(lr_exp,rep))
 
-            print('Training %s:'%(activation.name))
+            for activation in NonLinearity:
 
-            model = InequalityModel(activation)
-            sgd = SGD(model.parameters(), lr=LR)
-            loss_fn = CrossEntropyLoss()
-            all_epoch = EPOCHS
-            
-            accuracies = [0]*all_epoch
+                print('Training %s:'%(activation.name))
 
-            for current_epoch in range(all_epoch):
-                model.train()
-                for idx, (train_x, train_label) in enumerate(train_loader):
-                    train_x = train_x
-                    train_label = train_label
-                    sgd.zero_grad()
-                    predict_y = model(train_x.float())
-                    loss = loss_fn(predict_y, train_label.long())
-                    loss.backward()
-                    sgd.step()
+                model = InequalityModel(activation)
+                sgd = SGD(model.parameters(), lr=10**-(lr_exp+1))
+                loss_fn = CrossEntropyLoss()
+                all_epoch = EPOCHS
+                
+                accuracies = [0]*all_epoch
 
-                all_correct_num = 0
-                all_sample_num = 0
+                for current_epoch in range(all_epoch):
+                    model.train()
+                    for idx, (train_x, train_label) in enumerate(train_loader):
+                        train_x = train_x
+                        train_label = train_label
+                        sgd.zero_grad()
+                        predict_y = model(train_x.float())
+                        loss = loss_fn(predict_y, train_label.long())
+                        loss.backward()
+                        sgd.step()
 
-                model.eval()
-                for idx, (test_x, test_label) in enumerate(test_loader):
-                    test_x = test_x
-                    test_label = test_label
-                    predict_y = model(test_x.float()).detach()
-                    predict_y = torch.argmax(predict_y, dim=-1)
-                    current_correct_num = predict_y == test_label
-                    all_correct_num += np.sum(current_correct_num.numpy(), axis=-1)
-                    all_sample_num += current_correct_num.shape[0]
+                    all_correct_num = 0
+                    all_sample_num = 0
 
-                acc = all_correct_num / all_sample_num
-                accuracies[current_epoch] = acc
-                print('\tFinished epoch %s (%.2f)'%(current_epoch+1, acc))
+                    model.eval()
+                    for idx, (test_x, test_label) in enumerate(test_loader):
+                        test_x = test_x
+                        test_label = test_label
+                        predict_y = model(test_x.float()).detach()
+                        predict_y = torch.argmax(predict_y, dim=-1)
+                        current_correct_num = predict_y == test_label
+                        all_correct_num += np.sum(current_correct_num.numpy(), axis=-1)
+                        all_sample_num += current_correct_num.shape[0]
 
-            print('Finished training\n')
+                    acc = all_correct_num / all_sample_num
+                    accuracies[current_epoch] = acc
+                    print('\tFinished epoch %s (%.2f)'%(current_epoch+1, acc))
+
+                print('Finished training\n')
+                pyplot.plot([x+1 for x in list(range(all_epoch))], accuracies, label=activation.name)
 
             pyplot.xlabel("Epoch")
             pyplot.ylabel("Accuracy")
-            pyplot.plot([x+1 for x in list(range(all_epoch))], accuracies, label=activation.name)
+            pyplot.legend(loc='best')
+            pyplot.title('LR: 10^-%d; Repetition #%d'%(lr_exp,rep))
 
-        pyplot.legend(loc='best')
-        if not os.path.isdir("plots"):
-            os.mkdir("plots")
-        pyplot.savefig('plots/inequality_%d.png'%(rep), bbox_inches='tight')
-        pyplot.clf()
+            if not os.path.isdir("plots"):
+                os.mkdir("plots")
+            pyplot.savefig('plots/inequality_%d_%d.png'%(lr_exp,rep), bbox_inches='tight')
+            pyplot.clf()
 
 
 def train_recurrent():
@@ -87,60 +91,62 @@ def train_recurrent():
     train_loader = DataLoader(train_dataset, batch_size=batch_size)
     test_loader = DataLoader(test_dataset, batch_size=batch_size)
 
-    for rep in range(REPS):
+    for lr_exp in range(1, LR_NUM+1):
 
-        print('\n-- Starting rep #%d --\n'%(rep))
+        for rep in range(REPS):
 
-        for recurrence in RecType:
+            print('\n-- Starting: lr 10^-%d, rep #%d --\n'%(lr_exp,rep))
 
-            print('Training %s:'%(recurrence.name))
+            for recurrence in RecType:
 
-            model = RecurrentModel(recurrence)
-            sgd = SGD(model.parameters(), lr=LR)
-            loss_fn = CrossEntropyLoss()
-            all_epoch = EPOCHS
-            accuracies = [0]*all_epoch
+                print('Training %s:'%(recurrence.name))
 
-            for current_epoch in range(all_epoch):
-                model.train()
-                for idx, (train_x, train_label) in enumerate(train_loader):
-                    train_x = train_x
-                    train_label = train_label
-                    sgd.zero_grad()
-                    predict_y = model(train_x.float())
-                    loss = loss_fn(predict_y, train_label.long())
-                    loss.backward()
-                    sgd.step()
+                model = RecurrentModel(recurrence)
+                sgd = SGD(model.parameters(), lr=10**-lr_exp)
+                loss_fn = CrossEntropyLoss()
+                all_epoch = EPOCHS
+                accuracies = [0]*all_epoch
 
-                all_correct_num = 0
-                all_sample_num = 0
+                for current_epoch in range(all_epoch):
+                    model.train()
+                    for idx, (train_x, train_label) in enumerate(train_loader):
+                        train_x = train_x
+                        train_label = train_label
+                        sgd.zero_grad()
+                        predict_y = model(train_x.float())
+                        loss = loss_fn(predict_y, train_label.long())
+                        loss.backward()
+                        sgd.step()
 
-                model.eval()
-                for idx, (test_x, test_label) in enumerate(test_loader):
-                    test_x = test_x
-                    test_label = test_label
-                    predict_y = model(test_x.float()).detach()
-                    predict_y =torch.argmax(predict_y, dim=-1)
-                    current_correct_num = predict_y == test_label
-                    all_correct_num += np.sum(current_correct_num.numpy(), axis=-1)
-                    all_sample_num += current_correct_num.shape[0]
+                    all_correct_num = 0
+                    all_sample_num = 0
 
-                acc = all_correct_num / all_sample_num
-                accuracies[current_epoch] = acc
-                print('\tFinished epoch %s (%.2f)'%(current_epoch+1, acc))
+                    model.eval()
+                    for idx, (test_x, test_label) in enumerate(test_loader):
+                        test_x = test_x
+                        test_label = test_label
+                        predict_y = model(test_x.float()).detach()
+                        predict_y =torch.argmax(predict_y, dim=-1)
+                        current_correct_num = predict_y == test_label
+                        all_correct_num += np.sum(current_correct_num.numpy(), axis=-1)
+                        all_sample_num += current_correct_num.shape[0]
 
-            print('Finished training\n')
+                    acc = all_correct_num / all_sample_num
+                    accuracies[current_epoch] = acc
+                    print('\tFinished epoch %s (%.2f)'%(current_epoch+1, acc))
+
+                print('Finished training\n')
+                pyplot.plot([x+1 for x in list(range(all_epoch))], accuracies, label=recurrence.name)
 
             pyplot.xlabel("Epoch")
             pyplot.ylabel("Accuracy")
-            pyplot.plot([x+1 for x in list(range(all_epoch))]. accuracies, label=recurrence.name)
+            pyplot.legend(loc='best')
+            pyplot.title('LR: 10^-%d; Repetition #%d'%(lr_exp,rep))
 
-        pyplot.legend(loc='best')
-        pyplot.title('Repetition #%d'%(rep))
-        if not os.path.isdir("plots"):
-            os.mkdir("plots")
-        pyplot.savefig('plots/recurrence_%d.png'%(rep), bbox_inches='tight')
-        pyplot.clf()
+            if not os.path.isdir("plots"):
+                os.mkdir("plots")
+            pyplot.savefig('plots/recurrence_%d_%d.png'%(lr_exp,rep), bbox_inches='tight')
+            pyplot.clf()
 
 def train_original():
     batch_size = 256
